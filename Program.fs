20cef98ce0211ad094d7f34c68b988d571212bd3
actuallyachraf <actuallyachraf@gmail.com>
2019-07-23 19:09:39 +0100

    add add and decrypt

--- Program.fs
+++ Program.fs
@@ -11,16 +11,28 @@ let main argv =
         | None -> emtpyPubkey, emptyPrivateKey
         | Some(a, b) -> a, b
 
-    let m = Math.BigInteger.ValueOf 9132L
+    let m1 = Math.BigInteger.ValueOf 77234725L
+    let m2 = Math.BigInteger.ValueOf  32234L
+    let a =
+        match encrypt pub (m1.ToByteArray()) with
+        | None -> [| 0uy |]
+        | Some(b) -> b
+    printfn "%A" (bigint a)
+    let b =
+        match encrypt pub (m2.ToByteArray()) with
+        | None -> [| 0uy |]
+        | Some(b) -> b
+    printfn "%A" (Math.BigInteger b)
 
-    let encM =
-        match encrypt pub (m.ToByteArray()) with
+    let sum = add pub (a,b)
+    printfn "%A" (Math.BigInteger sum)
+
+    let decSum =
+        match decrypt priv sum with
         | None -> [| 0uy |]
         | Some(b) -> b
-    printfn "%A" (bigint encM)
-    let decM =
-        match decrypt priv encM with
-        | None -> [| 0uy |]
-        | Some(b) -> b
-    printfn "%A" (Math.BigInteger decM)
+    printfn "%A" (Math.BigInteger decSum)
+
+    printfn "%A" (decSum = (m1.Add m2).ToByteArray() )
+
     0 // return an integer exit code
