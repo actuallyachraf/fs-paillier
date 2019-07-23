ea368e8e4233ac16b6cc35d4c76b755b04e0364a
actuallyachraf <actuallyachraf@gmail.com>
2019-07-23 03:00:20 +0100

    encrypt/decrypt

--- Paillier.fs
+++ Paillier.fs
@@ -2,53 +2,83 @@ module Paillier
 
 open Org.BouncyCastle
 
-
 let one = Math.BigInteger.One
 
 type Publickey =
-    {
-        Length : int // length in bits
-        N : Math.BigInteger
-        G : Math.BigInteger
-        Nsquared : Math.BigInteger }
+    { Len : int // length in bits
+      N : Math.BigInteger
+      G : Math.BigInteger
+      Nsquared : Math.BigInteger }
 
 type PrivateKey =
-    {
-        Length : int // length in bits
-        Pubkey : Publickey
-        L : Math.BigInteger
-        U : Math.BigInteger }
+    { Length : int // length in bits
+      Pubkey : Publickey
+      L : Math.BigInteger
+      U : Math.BigInteger }
 
+let urandom = Security.SecureRandom()
 
+let emtpyPubkey =
+    { Len = 0
+      N = one
+      G = one
+      Nsquared = one }
 
-// isPrime
-let private isprime p =
-    let rand = Org.BouncyCastle.Security.SecureRandom ()
-    Math.Primes.IsMRProbablePrime (p,rand,100)
+let emptyPrivateKey =
+    { Length = 0
+      Pubkey = emtpyPubkey
+      L = one
+      U = one }
 
-let urandom = Security.SecureRandom ()
+let private isprime p = Math.Primes.IsMRProbablePrime(p, urandom, 100)
+let private randomPrime bits = Math.BigInteger.ProbablePrime(bits, urandom)
 
-let private randomPrime bits = Math.BigInteger.ProbablePrime (bits/2 ,Security.SecureRandom ())
 // genkeypair
 let genkeypair bits =
-
-    let p = Math.BigInteger.ProbablePrime (bits/2 ,urandom)
-    let q = Math.BigInteger.ProbablePrime (bits/2 ,urandom)
-
-    if (isprime p || isprime q)
-        then
-
+    let p = randomPrime (bits / 2)
+    let q = randomPrime (bits / 2)
+    if (isprime p && isprime q) then
         let n = p.Multiply q
         let nsquare = n.Multiply n
-
         let g = n.Add one
         let pMinus = p.Subtract one
         let qMinus = q.Subtract one
         let l = pMinus.Multiply qMinus
         let u = l.ModInverse n
 
-        let pk = {Length = bits;N=n;Nsquared=nsquare;G=g}
-        let sk = {Length = bits;Pubkey=pk;L=l;U=u}
-        Some(pk,sk)
-        else
-        None
+        let pk =
+            { Len = bits
+              N = n
+              Nsquared = nsquare
+              G = g }
+
+        let sk =
+            { Length = bits
+              Pubkey = pk
+              L = l
+              U = u }
+
+        Some(pk, sk)
+    else None
+
+let encrypt pubkey (message : byte []) =
+    let r = randomPrime pubkey.Len
+    let m = Math.BigInteger message
+    if pubkey.N.CompareTo(m) < 1 then None
+    else
+        let gm = pubkey.G.ModPow(m, pubkey.Nsquared)
+        let rn = r.ModPow(pubkey.N, pubkey.Nsquared)
+        let prod = gm.Multiply rn
+        let c = prod.Mod pubkey.Nsquared
+        Some(c.ToByteArray())
+
+let decrypt privatekey (cipher : byte []) =
+    let c = Math.BigInteger cipher
+    if privatekey.Pubkey.Nsquared.CompareTo c < 1 then None
+    else
+        let a = c.ModPow(privatekey.L, privatekey.Pubkey.Nsquared)
+        let l = a.Subtract one
+        let l = l.Divide privatekey.Pubkey.N
+        let m = l.Multiply privatekey.U
+        let m = m.Mod privatekey.Pubkey.N
+        Some(m.ToByteArray())
--- Program.fs
+++ Program.fs
@@ -1,13 +1,26 @@
 ﻿// Learn more about F# at http://fsharp.org
-
 open System
 open Paillier
+open Org.BouncyCastle
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let pub, priv =
+        match genkeypair 1024 with
+        | None -> emtpyPubkey, emptyPrivateKey
+        | Some(a, b) -> a, b
 
-    match genkeypair 1024 with
-    | None -> printfn "failed to generate primes"
-    | Some(a,b) -> (printfn "Public Key : %A \n PrivateKey : %A "  a b)
+    let m = Math.BigInteger.ValueOf 9132L
+
+    let encM =
+        match encrypt pub (m.ToByteArray()) with
+        | None -> [| 0uy |]
+        | Some(b) -> b
+    printfn "%A" (bigint encM)
+    let decM =
+        match decrypt priv encM with
+        | None -> [| 0uy |]
+        | Some(b) -> b
+    printfn "%A" (Math.BigInteger decM)
     0 // return an integer exit code
